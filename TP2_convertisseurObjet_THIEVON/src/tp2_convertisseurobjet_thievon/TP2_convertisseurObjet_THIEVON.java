c9715b878af4834f7498486f729169d500f0beed
Francois Thievon <francois.thievon@epfedu.fr>
2022-10-25 00:05:03 +0200

    TP convertisseur objet avancement perso maison

--- TP2_convertisseurObjet_THIEVON/src/tp2_convertisseurobjet_thievon/TP2_convertisseurObjet_THIEVON.java
+++ TP2_convertisseurObjet_THIEVON/src/tp2_convertisseurobjet_thievon/TP2_convertisseurObjet_THIEVON.java
@@ -15,6 +15,39 @@ public class TP2_convertisseurObjet_THIEVON {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        
+        int resp;
+        float nb;
+        Scanner sc;
+        sc = new Scanner(System.in);
+        System.out.println("Bonjour, veuillez saisir une valeure");
+        nb = sc.nextFloat();
+        System.out.println("Veuillez maintenant choisir le convertisseur en saisissant :");
+        System.out.println("(1) pour convertir des celcius en kelvin");
+        System.out.println("(2) pour convertir des kelvin en celcius");
+        System.out.println("(3) pour convertir des farenheit en celcius");
+        System.out.println("(4) pour convertir des celcius en farenheit");
+        System.out.println("(5) pour convertir des farenheit en kelvin");
+        System.out.println("(6) pour convertir des kelvin en farenheit");
+        resp = sc.nextInt();
+        if (resp == 1) {
+            CelciusVersKelvin(nb);
+        }
+        if (resp == 2) {
+            KelvinVersCelcius(nb);
+        }
+        if (resp == 3) {
+            FarenheitVersCelcius(nb);
+        }
+        if (resp == 4) {
+            CelciusVersFarenheit(nb);
+        }
+        if (resp == 5) {
+            FarenheitVersKelvin(nb);
+        }
+        if (resp == 6) {
+            KelvinVersFarenheit(nb);
+        }
     }
     
 }
