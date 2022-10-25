1e3544234bf79125a1d9f66b638f0885ff95e916
Francois Thievon <francois.thievon@epfedu.fr>
2022-10-25 22:20:04 +0200

    TP2 relation 1 terminé avec commentaires et tout les tests de la méthode ajouter_voiture()

--- TP2_relation_1_THIEVON/src/tp2_relation_1_thievon/TP2_relation_1_THIEVON.java
+++ TP2_relation_1_THIEVON/src/tp2_relation_1_thievon/TP2_relation_1_THIEVON.java
@@ -20,12 +20,14 @@ public class TP2_relation_1_THIEVON {
         Voiture uneAutreClio = new Voiture ("Clio", "Renault", 5 ) ;
         Voiture une2008 = new Voiture ("2008", "Peugeot", 6 ) ;
         Voiture uneMicra = new Voiture ("Micra", "Nissan", 4 ) ;
+        Voiture uneMultiplat = new Voiture ("Multiplat", "Fiat", 3 ) ;
+        Voiture uneMaserati = new Voiture ("Granturismo", "Maserati", 10 ) ;
         
         // On initialise 4 voitures
         
         Personne Bob = new Personne ("Bobby", "Sixkiller");
         Personne Reno = new Personne ("Reno", "Raines");
-        System.out.println("liste des voitures disponibles :\n"+ uneClio + "\n" + uneAutreClio + "\n" + une2008 + "\n" + uneMicra + "\n" ) ;
+        System.out.println("liste des voitures disponibles :\n"+ uneClio + "\n" + uneAutreClio + "\n" + une2008 + "\n" + uneMicra + "\n" + uneMultiplat + "\n" + uneMaserati + "\n") ;
         
         // On initialise 2 personnes et on affiche la liste des voitures
         
@@ -38,8 +40,9 @@ public class TP2_relation_1_THIEVON {
         
         // On crée des relations entre Bob et 2 voitures
         
-        System.out.println("la premiere voiture de Bob est une" + Bob.liste_voitures[0] ) ;
-        System.out.println("la deuxieme voiture de Bob est une" + Bob.liste_voitures[1] ) ;
+        System.out.println("la premiere voiture de Bob est une " + Bob.liste_voitures[0] ) ;
+        System.out.println("la deuxieme voiture de Bob est une " + Bob.liste_voitures[1] ) ;
+        System.out.println("\n");
         
         // On affiche les voitures de Bob
         
@@ -52,12 +55,22 @@ public class TP2_relation_1_THIEVON {
         
         // On crée des relations entre Reno et 2 voitures
 
-        System.out.println("la premiere voiture de Reno est une" + Reno.liste_voitures[0] ) ;
-        System.out.println("la deuxieme voiture de Reno est une" + Reno.liste_voitures[1] ) ;
+        System.out.println("la premiere voiture de Reno est une " + Reno.liste_voitures[0] ) ;
+        System.out.println("la deuxieme voiture de Reno est une " + Reno.liste_voitures[1] ) ;
+        System.out.println("\n");
         
         // On affiche les voitures de Reno
         
+        Bob.ajouter_voiture(uneMultiplat);
+        Bob.ajouter_voiture(uneMaserati);
         Bob.ajouter_voiture(uneAutreClio);
+        
+        // On ajoute la Fiat Multiplat et la Maserati Granturismo à Bob grace à la méthode ajouter voiture
+        
+        System.out.println("\n");
+        System.out.println("la premiere voiture de Bob est une " + Bob.liste_voitures[0] ) ;
+        System.out.println("la deuxieme voiture de Bob est une " + Bob.liste_voitures[1] ) ;
+        System.out.println("la troisieme voiture de Bob est une " + Bob.liste_voitures[2] ) ;
     }
     
 }
