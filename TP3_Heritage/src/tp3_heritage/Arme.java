3616e7f4bdf60d8bf537c673a0f608512d15d488
Francois Thievon <francois.thievon@epfedu.fr>
2022-11-08 17:09:11 +0100

    TP3_Heritage début (Séance 1 TD création classes arme baton epee)

---
+++ TP3_Heritage/src/tp3_heritage/Arme.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package tp3_heritage;
+
+/**
+ *
+ * @author thiev
+ */
+public class Arme {
+    String nom;
+    int lvlAttaque;  
+    
+
+
+public Arme(String unNom, int lvla) {
+    nom = unNom;
+    lvlAttaque = lvla;
+}
+
+@Override
+public String toString() {
+    String chaine_a_retourner;
+    chaine_a_retourner = nom + " (" + lvlAttaque + "points d'attaques";
+    return chaine_a_retourner ;
+}
+}
---
+++ TP3_Heritage/src/tp3_heritage/Baton.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package tp3_heritage;
+
+/**
+ *
+ * @author thiev
+ */
+public class Baton extends Arme {
+    int age;
+    
+    public Baton(String n, int i, int a) {
+        super(n, i);
+        age = a;
+    }
+}
