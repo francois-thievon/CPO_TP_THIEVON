dba3f10fe4504eb73d60018bbced215bb759bc8e
Francois Thievon <francois.thievon@epfedu.fr>
2022-11-18 10:34:56 +0100

    SP4_Console_THIEVON objet Jeton fonctionnel

---
+++ SP4_console_THIEVON/src/sp4_console_thievon/SP4_console_THIEVON.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package sp4_console_thievon;
+
+/**
+ *
+ * @author thiev
+ */
+public class SP4_console_THIEVON {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        Jeton a = new Jeton("rouge");
+        System.out.println(a);
+    }
+    
+}
